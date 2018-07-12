4df52fae5f2774c03def60e033963550388953e2
George <george@georgekaplan.xyz>
2018-07-12 11:15:27 -0400

    move over, Kent Beck
    I am proud to announce that I have implemented a failing test

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,17 @@ pub fn in_polygon(query_point: na::Vector2<f64>, bounds: Vec<na::Vector2<f64>>) 
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn center_of_square() {
+        let square_pts = vec![
+            na::Vector2::new(-1.0, 1.0),
+            na::Vector2::new(1.0, 1.0),
+            na::Vector2::new(1.0, -1.0),
+            na::Vector2::new(-1.0, -1.0),
+        ];
+        let query_pt = na::Vector2::new(0.0, 0.0);
+        assert_eq!(in_polygon(query_pt, square_pts), true);
     }
 }
