19c831d51664f877e8cdd5ee7e02e1491ce44f1f
George <george@georgekaplan.xyz>
2018-07-12 11:50:54 -0400

    enhanced bounding box test and moved it into its own function

--- src/lib.rs
+++ src/lib.rs
@@ -5,21 +5,35 @@ extern crate nalgebra as na;
 
 pub fn in_polygon(query_point: na::Vector2<f64>, bounds: Vec<na::Vector2<f64>>) -> bool {
     // the simplest metric of inclusion
+    if !in_bbox(&query_point, &bounds) {
+        return false;
+    }
+
+    true
+}
+
+// Test if a point lies inside the bounding box of a collection of points.
+fn in_bbox(query_point: &na::Vector2<f64>, bounds: &Vec<na::Vector2<f64>>) -> bool {
+    // constants
     let nan = 0. / 0.;
+    let inf = 1. / 0.;
+    let n_inf = -1. / 0.;
+
+    // find bounds
     let min_x = bounds.iter().map(|e| e[0]).fold(nan, f64::min);
     let max_x = bounds.iter().map(|e| e[0]).fold(nan, f64::max);
     let min_y = bounds.iter().map(|e| e[1]).fold(nan, f64::min);
     let max_y = bounds.iter().map(|e| e[1]).fold(nan, f64::max);
 
-    if query_point[0] < min_x
-        || query_point[0] > max_x
-        || query_point[1] < min_y
-        || query_point[1] > max_y
-    {
-        return false;
-    }
-
-    true
+    // decide if it's in or out
+    query_point[0] != inf
+        && query_point[0] != n_inf
+        && query_point[1] != inf
+        && query_point[1] != n_inf
+        && query_point[0] >= min_x
+        && query_point[0] <= max_x
+        && query_point[1] >= min_y
+        && query_point[1] <= max_y
 }
 
 #[cfg(test)]
