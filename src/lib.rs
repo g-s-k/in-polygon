47ca471582e3894fb32175fc4cc6ee26bd2ff83a
George <george@georgekaplan.xyz>
2018-07-12 16:45:16 -0400

    implemented diff and began imitating reference paper

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 extern crate nalgebra as na;
 
 use std::f64::NAN;
+use std::f64::consts::PI;
 use std::f64::INFINITY as INF;
 use std::f64::NEG_INFINITY as N_INF;
 
@@ -13,7 +14,27 @@ pub fn in_polygon(query_point: na::Point2<f64>, bounds: Vec<na::Point2<f64>>) ->
         return false;
     }
 
+    // TODO: close `bounds`'s path if it is open. Not required for MWE, but important.
+
+    // move query point to origin
+    let shifted = bounds.iter().map(|pt| pt - query_point);
+    // get quadrant
+    let quadrant = shifted.map(|pt| (pt[0].atan2(pt[1]) * 2. / PI).floor());
+    // diff them
+    let quad_diff = diff(&quadrant);
+
     true
+}
+
+/// Compute discrete differential from an iterator of values.
+fn diff<I, T>(pts: &I) -> impl Iterator<Item = T>
+where
+    I: Iterator<Item = T> + Clone,
+    T: std::ops::Sub<Output = T>
+{
+    let pts_shift = pts.clone().cycle().skip(1);
+    let paired = pts.clone().zip(pts_shift);
+    paired.map(|(pt1, pt2)| pt2 - pt1)
 }
 
 /// Get the bounding box of a slice of 2D points.
