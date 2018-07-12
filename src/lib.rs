add620e34280d872b42305c42769b8ec00681ae1
George <george@georgekaplan.xyz>
2018-07-12 11:31:24 -0400

    made the test pass
    Don't get too excited, there is a lot more work to do.

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,22 @@
 extern crate nalgebra as na;
 
 pub fn in_polygon(query_point: na::Vector2<f64>, bounds: Vec<na::Vector2<f64>>) -> bool {
-    false
+    // the simplest metric of inclusion
+    let nan = 0. / 0.;
+    let min_x = bounds.iter().map(|e| e[0]).fold(nan, f64::min);
+    let max_x = bounds.iter().map(|e| e[0]).fold(nan, f64::max);
+    let min_y = bounds.iter().map(|e| e[1]).fold(nan, f64::min);
+    let max_y = bounds.iter().map(|e| e[1]).fold(nan, f64::max);
+
+    if query_point[0] < min_x
+        || query_point[0] > max_x
+        || query_point[1] < min_y
+        || query_point[1] > max_y
+    {
+        return false;
+    }
+
+    true
 }
 
 #[cfg(test)]
