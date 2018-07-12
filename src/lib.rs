bc84c2bf8df2b167305677cad3405d0521aa3458
George <george@georgekaplan.xyz>
2018-07-12 10:59:03 -0400

    new crate

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+//! Given a list of vertices, determine whether a point lies inside the polygon
+//! they bound.
+
+extern crate nalgebra as na;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
